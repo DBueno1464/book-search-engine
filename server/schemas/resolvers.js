655e50a1243dce2c27a966cc3a817e458147dd6b
DBueno1464 <DerrickBueno00@gmail.com>
2021-11-03 17:52:30 -0400

    added query to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,22 @@
+const { AuthenticationError } = require('apollo-server-errors');
+const { getSingleUser } = require('../controllers/user-controller');
 const { Book, User } = require('../models');
 
+const resolvers = {
+    Query: {
+        // me: async (parent, args, context) => {
+        //     if (context.user) {
+        //         return User.findOne({ _id: context.user._id })
+        //     }
+        //     throw new AuthenticationError('You need to be logged in!');
+        // }
+        me: async (parent, args, context) => {
+            getSingleUser(context, res);
+        }
+    }
+
+    Mutation: {
+
+    }
+}
 module.exports = resolvers;
