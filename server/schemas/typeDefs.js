a1e514f6b0fa8922d1c5a41566277bdecbe30b2c
DBueno1464 <DerrickBueno00@gmail.com>
2021-11-03 17:29:38 -0400

    updated image and link query types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,8 +14,8 @@ const typeDefs = gql`
         authors: [Auth]
         description: String
         title: String
-        image: 
-        link: 
+        image: String
+        link: String
     }
 
     type Auth {
@@ -30,7 +30,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(author: [String]!, description: String!, title: String!, bookId: Int!, image:!,link:!): User
+        saveBook(author: [String]!, description: String!, title: String!, bookId: Int!, image: String!, link: String!): User
         removeBook(bookId: Int!): User
     }
 `
