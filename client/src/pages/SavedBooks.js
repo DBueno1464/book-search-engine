ed07d0fc2d32a800ae8e9c87996eed87cdf6d86a
DBueno1464 <DerrickBueno00@gmail.com>
2021-11-08 02:52:59 -0500

    added queries and mutations to pages

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -5,7 +5,7 @@ import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 
-import { getMe, deleteBook } from '../utils/API';
+// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
