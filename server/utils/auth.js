21320deb730f15fe2b3d65e419432801ac6ba8b4
DBueno1464 <DerrickBueno00@gmail.com>
2021-11-08 18:40:48 -0500

    fixed serever 500 error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,8 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
+    console.log(req);
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
